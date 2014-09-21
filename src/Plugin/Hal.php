0bb9eb0bdbf1cdb15b4ae53e1a3dc1cf0295a106
Corentin Larose <corentin.larose@qapa.com>
2014-09-21 11:21:04 +0200

    Added renderCollection.post event

--- src/Plugin/Hal.php
+++ src/Plugin/Hal.php
@@ -455,6 +455,8 @@ class Hal extends AbstractHelper implements
             $payload['total_items'] = isset($payload['total_items']) ? $payload['total_items'] : count($collection);
         }
 
+        $this->getEventManager()->trigger(__FUNCTION__  . 'post', $this, array('payload' => $payload));
+
         return $payload;
     }
 
